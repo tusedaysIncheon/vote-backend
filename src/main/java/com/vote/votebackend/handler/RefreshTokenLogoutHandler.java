a943c54e4a92b94afbdb5484f98a42a567ba4169
Jeonjaeman <wjswoaks741@gmail.com>
2025-10-28 23:47:27 +0900

    Spring Security 기본 로그아웃 체인에 커스텀 RefreshTokenLogoutHandler를 추가해 요청 바디의 Refresh 토큰을 제거.

---
+++ src/main/java/com/vote/votebackend/handler/RefreshTokenLogoutHandler.java
@@ -0,0 +1,54 @@
+package com.vote.votebackend.handler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.openid.connect.sdk.LogoutRequest;
+import com.vote.votebackend.domain.jwt.service.JwtService;
+import com.vote.votebackend.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@RequiredArgsConstructor
+public class RefreshTokenLogoutHandler implements LogoutHandler {
+
+    private final JwtService jwtService;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        try {
+
+//            String body = new BufferedReader(new InputStreamReader((request.getInputStream())))
+//                    .lines().reduce("", String::concat);
+
+            String body = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+            if (!StringUtils.hasText(body)) return;
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(body);
+            String refreshToken = jsonNode.has("refresh_token") ? jsonNode.get("refresh_token").asText() : null;
+
+            if (refreshToken == null) return;
+
+            Boolean isValid = JWTUtil.isValid(refreshToken, false);
+
+            if(isValid) return;
+
+            jwtService.removeRefresh(refreshToken);
+
+        } catch (IOException e) {
+            throw new RuntimeException("리프레쉬토큰을 읽어오는데 실패하였습니다.",e);
+        }
+    }
+}
